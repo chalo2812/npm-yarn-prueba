33e1cddcf637ce83ff3980cf60f8a20c66bbb95f
gonzalosola@gmail.com <gfsola@afip.gob.ar>
2018-11-05 13:19:59 -0300

    mejoras

--- build/dev-server.js
+++ build/dev-server.js
@@ -23,7 +23,7 @@ var devMiddleware = require('webpack-dev-middleware')/*(compiler, {
   quiet: true
 })*/
 
-var hotMiddleware = require('webpack-hot-middleware')/*(compiler, {
+/*var hotMiddleware = require('webpack-hot-middleware')/*(compiler, {
   log: () => {}
 })*/
 // force page reload when html-webpack-plugin template changes
